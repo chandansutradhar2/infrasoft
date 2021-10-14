8c7cbc4d82e24f69ad62eebd089db961656ebd26
chandan_naresh <chandansutradhar2@gmail.com>
2021-10-14 12:38:39 +0530

    creating nav catgories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { NavMenu } from './models/menu.model';
 import { LOGIN_TYPE } from './models/user.model';
 
 @Component({
@@ -9,5 +10,31 @@ import { LOGIN_TYPE } from './models/user.model';
 export class AppComponent {
   loginType = LOGIN_TYPE.admin;
 
+  adminNavCategories: NavMenu[] = [
+    { url: '', name: 'Dashboard', icon: '' },
+    { url: '', name: 'Manage Seller', icon: '' },
+    { url: '', name: 'Manger User', icon: '' },
+    { url: '', name: 'Financial', icon: '' },
+    { url: '', name: 'Order Tracking', icon: '' },
+    { url: '', name: 'Reports', icon: '' },
+  ];
+
+  sellerNavCategories: NavMenu[] = [
+    { url: '', name: 'Dashboard', icon: '' },
+    { url: '', name: 'Add Product', icon: '' },
+    { url: '', name: 'Delete Product', icon: '' },
+    { url: '', name: 'Order Management', icon: '' },
+    { url: '', name: 'Rating & Review', icon: '' },
+    { url: '', name: 'Manage Business', icon: '' },
+  ];
+
+  userNavCategories: NavMenu[] = [
+    { url: '', name: 'Home', icon: '' },
+    { url: '', name: 'Shop', icon: '' },
+    { url: '', name: 'Features', icon: '' },
+    { url: '', name: 'Blog', icon: '' },
+    { url: '', name: 'About', icon: '' },
+    { url: '', name: 'Contact', icon: '' },
+  ];
   constructor() {}
 }
---
+++ src/app/models/menu.model.ts
@@ -0,0 +1,5 @@
+export interface NavMenu {
+  url?: string;
+  name: string;
+  icon?: string;
+}
