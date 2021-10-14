9b029efc0079c7090c69e02b021a3f6dc7f8ec46
chandan_naresh <chandansutradhar2@gmail.com>
2021-10-14 10:01:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'e-com';
+
+  search: string = 'Search By Category';
+
+  constructor() {}
+
+  setSearchString() {
+    //todo go to db , and generate sreahc placeholder
+    this.search = 'Got from Server';
+  }
 }
