7f64ec4b079ecd9dbcce32fc38fb960f28f495a5
chandan_naresh <chandansutradhar2@gmail.com>
2021-10-18 10:27:12 +0530

    signup UI using bootstrap

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LOGIN_TYPE } from 'src/app/models/user.model';
 
+//decorator -metadata
 @Component({
   selector: 'cn-signup',
   templateUrl: './signup.component.html',
-  // styleUrls: ['./signup.component.scss'],
-  styles: [
-    `
-      background-color: red;
-    `,
-  ],
+  styleUrls: ['./signup.component.scss'],
+  // styles: [
+  //   `
+  //     background-color: red;
+  //   `,
+  // ],
 })
 export class SignupComponent {
-  constructor() {
-    alert('signup loaded');
-  }
+  @Input() userType: string = LOGIN_TYPE.user;
+
+  constructor() {}
 }
