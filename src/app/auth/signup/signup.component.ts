92c487eca5b4d1de34b84b11474fea46e961c0eb
chandan_naresh <chandansutradhar2@gmail.com>
2021-10-18 10:53:17 +0530

    interpolation using method

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,6 +14,23 @@ import { LOGIN_TYPE } from 'src/app/models/user.model';
 })
 export class SignupComponent {
   @Input() userType: string = LOGIN_TYPE.user;
+  maxTime: number = 10;
+  timer: string = `you have ${this.maxTime} minutes`;
+  currentTime!: Date;
+  constructor() {
+    this.currentTime = new Date();
+    this.remainingTime();
+  }
 
-  constructor() {}
+  remainingTime() {
+    setInterval(() => {
+      this.maxTime = this.maxTime - 1;
+      this.timer = `you have ${this.maxTime} minutes`;
+      this.printTimeLeft();
+    }, 60000);
+  }
+
+  printTimeLeft(): string {
+    return this.timer;
+  }
 }
